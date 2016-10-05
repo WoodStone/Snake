1c872f982960340949cbff091d3985c2467f35d4
Vestein Dahl <vesteindahl@hotmail.com>
2016-10-05 22:45:05 +0200

    Adds new methods to InputController.

--- core/src/main/java/no/vestein/snake/input/InputController.java
+++ core/src/main/java/no/vestein/snake/input/InputController.java
@@ -1,20 +1,48 @@
 package no.vestein.snake.input;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
+import no.vestein.snake.Updatable;
+import no.vestein.snake.world.WorldController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class InputController extends InputAdapter {
+public class InputController extends InputAdapter implements Updatable {
 
+  private final WorldController world;
   private Map<Integer, Action> inputMap = new HashMap<>();
+  private Map<Integer, Boolean> continousInputList = new HashMap<>();
+
+  public InputController(final WorldController world) {
+    this.world = world;
+  }
 
   @Override
   public boolean keyDown(int keyCode) {
     if (inputMap.containsKey(keyCode)) {
       inputMap.get(keyCode).call();
     }
+    world.getScriptManager().executeKeyPressed(Input.Keys.toString(keyCode), world);
+
+    continousInputList.remove(keyCode, true);
     return false;
+  }
+
+  @Override
+  public boolean keyUp(int keyCode) {
+    continousInputList.remove(keyCode, false);
+    return false;
+  }
+
+  public void enableContinousInput(final int keyCode) {
+    if (!continousInputList.containsKey(keyCode)) {
+      continousInputList.put(keyCode, false);
+    }
+  }
+
+  public void disableContinousInput(final int keyCode) {
+    continousInputList.remove(keyCode);
   }
 
   public void registerInput(final int keyCode, Action action) {
@@ -22,6 +50,15 @@ public class InputController extends InputAdapter {
       throw new IllegalArgumentException(String.format("Key, %d, already registered.", keyCode));
     }
     inputMap.put(keyCode, action);
+  }
+
+  @Override
+  public void update(float deltaTime) {
+    for (int n : continousInputList.keySet()) {
+      if (continousInputList.get(n)) {
+        keyDown(n);
+      }
+    }
   }
 
 }
